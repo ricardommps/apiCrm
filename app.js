8488302cfd6c07495d4af99ee9c427fe0777d325
Ricardo Matta <Ricardo Matta>
2017-02-14 09:59:09 -0200

    alterações

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ var teste = require('./routes/teste');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.set('view engine', 'html');
 // log to console
 app.use(morgan('dev'));
 
